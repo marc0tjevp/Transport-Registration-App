f8d445f76dca501104cd84acacee43239be3eb50
imdutch21 <bartpklomp@gmail.com>
2018-05-25 17:30:41 +0200

    Added a barcode scanner activity and started the freight activity

--- app/src/main/java/theekransje/douaneapp/Controllers/FreightActivity.java
+++ app/src/main/java/theekransje/douaneapp/Controllers/FreightActivity.java
@@ -1,16 +1,33 @@
 package theekransje.douaneapp.Controllers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import theekransje.douaneapp.R;
 
 public class FreightActivity extends AppCompatActivity {
-    private static final String TAG = "FreightActivity";
-
+    private static final int SCAN_BARCODE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.vracht_main);
+        setContentView(R.layout.activity_freight);
+    }
+
+    public void scan(View view) {
+        startActivityForResult(new Intent(this, BarcodeScannerActivity.class), SCAN_BARCODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case SCAN_BARCODE:{
+                if (requestCode == RESULT_OK){
+
+                }
+            }
+        }
     }
 }
