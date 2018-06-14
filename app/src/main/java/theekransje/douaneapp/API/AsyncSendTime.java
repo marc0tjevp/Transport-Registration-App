41def15263c7a85b1c6f0da1cd0f0e1a19e11699
MrSpaceblaze <32390661+MrSpaceblaze@users.noreply.github.com>
2018-06-14 11:53:08 +0200

    Made it possible to send Data to the API

--- app/src/main/java/theekransje/douaneapp/API/AsyncSendTime.java
+++ app/src/main/java/theekransje/douaneapp/API/AsyncSendTime.java
@@ -2,9 +2,16 @@ package theekransje.douaneapp.API;
 
 import android.os.AsyncTask;
 
+import org.json.JSONObject;
+
+import java.net.HttpURLConnection;
+import java.util.Date;
+
+import theekransje.douaneapp.Controllers.DrivingState;
+
 public class AsyncSendTime extends AsyncTask {
     private static final String TAG = "AsyncSendTime";
-    private final String endPoint = "/tbd";
+    private final String endPoint = "/senddrive";
     private ApiHelper helper;
 
     public AsyncSendTime(){
@@ -13,7 +20,25 @@ public class AsyncSendTime extends AsyncTask {
 
     @Override
     protected Object doInBackground(Object[] objects) {
+        try {
+            HttpURLConnection conn = helper.getConnection();
+            JSONObject object = new JSONObject();
+            object.put("startTime", ((Date[]) objects[0])[0].toString());
+            object.put("endTime", ((Date[]) objects[0])[1].toString());
+            switch ((DrivingState) objects[1]) {
+                case Paused:object.put("type","Pauze");
+                    break;
+                case Driving:object.put("type","Rijden");
+                    break;
+                case Stopped:object.put("type","Rijden");
+                    break;
+            }
+            object.put("mrn",objects[2]);
+            object.put("driverID",objects[3]);
+            conn.getOutputStream().write(object.toString().getBytes());
+        } catch (Exception e){
 
+        }
         return null;
     }
 }
