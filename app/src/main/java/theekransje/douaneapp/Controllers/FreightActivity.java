f03a013b39884ad1823f6b03c14d449f3f3da566
SPostma <s.postma1994@gmail.com>
2018-06-19 09:38:17 +0200

    Controllers unfuck

--- app/src/main/java/theekransje/douaneapp/Controllers/FreightActivity.java
+++ app/src/main/java/theekransje/douaneapp/Controllers/FreightActivity.java
@@ -104,6 +104,7 @@ public class FreightActivity extends AppCompatActivity implements BottomNavigati
         onQueryTextChange("");
         list.setVisibility(View.GONE);
 
+        navigation.setVisibility(View.INVISIBLE);
 
 
         new AsyncGetFreights(this,driver).execute();
